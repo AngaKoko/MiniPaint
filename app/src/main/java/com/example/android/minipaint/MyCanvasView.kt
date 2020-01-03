d4d30e4c18729762e059f44093285f8cb8586a17
Anga <angakoko@gmail.com>
2020-01-03 11:39:06 +0100

    Create new bitmap and canvas on size change

--- app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
+++ app/src/main/java/com/example/android/minipaint/MyCanvasView.kt
@@ -1,8 +1,27 @@
 package com.example.android.minipaint
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
+import androidx.core.content.res.ResourcesCompat
 
 class MyCanvasView(context:Context): View(context){
 
+    private lateinit var extraCanvas: Canvas
+    private lateinit var extraBitmap: Bitmap
+
+    private val backgroundColor = ResourcesCompat.getColor(resources, R.color.colorBackground, null)
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        //Recycle old bitmap to prevent memory leak
+        if (::extraBitmap.isInitialized) extraBitmap.recycle()
+        //Create new bitmap and canvas on size changed
+        extraBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+        extraCanvas = Canvas(extraBitmap)
+        extraCanvas.drawColor(backgroundColor)
+
+    }
 }
